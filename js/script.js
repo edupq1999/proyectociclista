b560c1d74d1e754565739c446874769acdb46657
edupq1999 <nivel.17@hotmail.com>
2023-07-13 05:12:04 -0500

    Se arreglo indice del carrusel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const bannerImgs = ['https://www.fundacionmapfre.org/media/educacion-divulgacion/prevencion/accidentes-mayores/te-mueves-bicicleta-consejos-basicos-seguridad-vial-ciclista-1194x585-1.jpg', 'https://portal.andina.pe/EDPfotografia3/Thumbnail/2016/12/20/000394220W.jpg', 'https://bicicleta.cdecomunicacion.es/wp-content/uploads/2023/02/diseno-sin-titulo-2023-02-16t141345479-1140x641.png', 'https://www.barriosvecinales.com.ar/wp-content/uploads/2021/09/CICLO.png']
-let bannerIndex = 1;
+let bannerIndex = 0;
 function desaparecerBanner() {
     const banner = document.getElementById('main_img');
     bannerIndex++;
