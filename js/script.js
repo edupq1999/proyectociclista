a49a7fd8b9f0196bfd6a9ccf9f6f91fa9ec987e6
edupq1999 <nivel.17@hotmail.com>
2023-07-13 06:09:36 -0500

    Se agrego el funcionamiento de la primera parte de investigacion y desarrollo

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ function aparecerBanner(){
 
 document.addEventListener("DOMContentLoaded", aparecerBanner);
 
+const investigacionArreglo1Text = ['Las ciclovías en Lima están destinadas exclusivamente al tránsito de bicicletas. Los peatones, vehículos motorizados u otros medios de transporte no están permitidos en estas vías.', 'Las bicicletas deben circular en el sentido indicado por la señalización en la ciclovía. Normalmente, se sigue el mismo sentido que el flujo vehicular en la calle adyacente.', 'Los ciclistas deben obedecer los semáforos y señales de tránsito ubicados en las intersecciones de las ciclovías. Deben detenerse en los semáforos rojos y avanzar en verde.', 'Los ciclistas deben mantener una velocidad adecuada y segura en las ciclovías, teniendo en cuenta las condiciones del entorno y la presencia de otros usuarios.', 'Los adelantamientos a otros ciclistas o peatones en la ciclovía deben realizarse de forma segura, respetando la distancia mínima y utilizando las señales adecuadas. Los giros deben señalizarse claramente para advertir a los demás usuarios.', 'En Lima, el uso de casco no es obligatorio para ciclistas mayores de edad, pero se recomienda encarecidamente su uso como medida de seguridad.'];
+const investigacionArreglo1Img = ['imagenes/img_regulaciones1.png','imagenes/img_regulaciones2.png','imagenes/img_regulaciones3.png','imagenes/img_regulaciones4.png','imagenes/img_regulaciones5.png','./imagenes/img_regulaciones6.png'];
+
+const regulacionesImg = document.getElementsByClassName('investigacion_regulaciones_img');
+const regulacionesText = document.getElementsByClassName('investigacion_regulaciones_text');
+
+function cambioRegulaciones(e) {
+    regulacionesImg[0].setAttribute('src', investigacionArreglo1Img[e.target.id]);
+    regulacionesText[0].innerHTML= investigacionArreglo1Text[e.target.id];
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const regulaciones = document.getElementsByClassName('investigacion_regulaciones_option_title');
+    for (let i = 0; i < regulaciones.length; i++) {
+        regulaciones[i].addEventListener('click', cambioRegulaciones);
+        regulaciones[i].setAttribute('id', i)
+    }
+})
+
+const investigacionArreglo2Text = [];
+const investigacionArreglo2Img = [];
